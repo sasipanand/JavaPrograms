71220ada5f82f08f32ba0810ed54cddaf902945a
sasipanand <42948120+sasipanand@users.noreply.github.com>
2018-09-03 19:25:39 -0400

    With words list
    With words list

---
+++ WordDictionary.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WordDictionary 
+{
+	
+	public static void doesFileExist(String path) 
+	{
+		try
+		{
+			File file = new File(path);
+			Scanner sc = new Scanner(file);
+			
+			System.out.println("File Contents: ");
+			
+			while (sc.hasNextLine())
+			{
+				System.out.println(sc.nextLine());
+			}
+				System.out.println();
+			
+			System.out.println("Words after spliting\n");
+			System.out.println();
+			
+			sc = new Scanner(file);
+			while (sc.hasNextLine())
+			{
+				String read = sc.nextLine();
+				if(read.length()==0) continue;
+				String[] splited = read.split("[,-]");
+				for(int i=0;i<splited.length;i++)
+				{
+					if(splited[i].length()==0) continue;
+					System.out.println(splited[i].trim());
+				}
+			}
+			
+			
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println(" The file name you entered is not found. Exiting program.");
+		}
+	}
+	
+	
+	public static void main(String[] args) 
+	{ 
+		Scanner sc = new Scanner(System.in);
+		String filename = "";
+		
+		System.out.print("Please enter a filename: ");
+		filename = sc.nextLine();
+		
+		doesFileExist(filename);
+		
+	}
+}
